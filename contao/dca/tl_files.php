7f8c1a5616fe009d75e1e839c43bc45f5315dc4d
Fritz Michael Gschwantner <fmg@inspiredminds.at>
2025-04-07 14:45:19 +0100

    add validation

--- contao/dca/tl_files.php
+++ contao/dca/tl_files.php
@@ -11,7 +11,7 @@ $GLOBALS['TL_DCA']['tl_files']['fields']['autoDeleteFilesTime'] = [
     'inputType' => 'inputUnit',
     'options' => ['hour', 'day', 'month', 'year'],
     'reference' => &$GLOBALS['TL_LANG']['tl_files']['autoDeleteFilesTimeOptions'],
-    'eval' => ['maxlength' => 200, 'tl_class' => 'w50 clr'],
+    'eval' => ['maxlength' => 200, 'rgxp' => 'natural', 'tl_class' => 'w50 clr'],
     'sql' => ['type' => 'string', 'length' => 255, 'default' => ''],
 ];
 
